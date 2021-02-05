5cbf8dfd1ab1b5259d2a570dd24c77d3a1c64c47
Meike Steinhilber <Meike.Steinhilber@aol.com>
2021-02-05 19:23:12 +0100

    test-delete_na is finished and the function is debugged.

--- R/delete_na.R
+++ R/delete_na.R
@@ -1,13 +1,23 @@
-delete_na <- function(x, y = NULL, wanted = NULL) {
-  if (!is.null(y) && (length(y) > 1)) {
-    whichNA <- is.na(x) | is.na(y)
-    x <- x[!whichNA]
-    y <- y[!whichNA]
-  } else if (y == 1 || (is.null(y))) {
-    whichNA <- is.na(x)
-    x <- x[!whichNA]
+delete_na <-  function(x,
+                       y = NULL,
+                       one_sample,
+                       paired = FALSE,
+                       wanted = NULL) {
+  if (one_sample == FALSE && paired == FALSE)
+  {
+    x <- x[!is.na(x)]
+    y <- y[!is.na(y)]
   } else {
-    x <- na.omit(x)
+    if (!is.null(y) && (length(y) > 1)) {
+      whichNA <- is.na(x) | is.na(y)
+      x <- x[!whichNA]
+      y <- y[!whichNA]
+    } else if (y == 1 || (is.null(y))) {
+      whichNA <- is.na(x)
+      x <- x[!whichNA]
+    } else {
+      x <- x[!is.na(x)]
+    }
   }
 
   if (wanted == "x") {
---
+++ tests/testthat/test-extract_formula.R
@@ -0,0 +1,9 @@
+context("extract_formula: Correct extraction of the formula.")
+
+
+test_that("extract_formula: Correct activation of errors", {
+
+  # expect_equal(x_test, 1:10)
+  # expect_equal(y_test, 1:10)
+
+})
