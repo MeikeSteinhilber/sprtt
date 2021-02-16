8612f4ba32a5b18f92b472e949f5b75088f78bcc
Meike Steinhilber <Meike.Steinhilber@aol.com>
2021-02-16 11:59:38 +0100

    start with the documentation

--- R/seq_ttest.R
+++ R/seq_ttest.R
@@ -11,12 +11,39 @@
 #######################################################################################
 #--- GENERAL SETTINGS
 
+
+
+
+#' Sequential t-Tests
+#'
+#' Performs one and two sample sequential t-tests on vectors of data.
+#'
+#' @param input1 Works with two classes: 'numeric' and 'formula'. Therfore you can
+#' write 'x' or 'x~y'.
+#' x: a (non-empty) numeric vector of data values.
+#' formula: a formula of the form lhs ~ rhs where lhs is a numeric variable giving
+#' the data values and rhs either 1 for a one-sample test or a factor with two levels
+#' giving the corresponding groups.
+#' @param y an optional (non-empty) numeric vector of data values.
+#' @param data an optional data.frame, which you can use only in comination with a
+#' formula input in 'input1'.
+#' @param mu a number indicating the true value of the mean (or difference in means
+#' if you are performing a two sample test).
+#' @param d a number indicating the specified effect size (Cohen's d)
+#' @param alpha the type I error. A number between 0 and 1.
+#' @param power 1 − β (β is the type II error probability). A number between 0 and 1.
+#' @param alternative a character string specifying the alternative hypothesis, must
+#' be one of "two.sided" (default), "greater" or "less". You can specify just the
+#' initial letter.
+#' @param paired a logical indicating whether you want a paired t-test.
+#'
+#' @return
+#' @export
+#',
 #' @import stats
 #' @importFrom methods callNextMethod new
-#' @export seq_ttest
-
-library(methods)
-
+#'
+#' @examples
 seq_ttest <- function(input1,
                     y = NULL,
                     data = NULL,
@@ -25,8 +52,8 @@ seq_ttest <- function(input1,
                     alpha = 0.05,
                     power = 0.95,
                     alternative = "two.sided",
-                    paired = FALSE,
-                    ...){
+                    paired = FALSE
+                    ){
   input1_name <- deparse(substitute(input1))
   if(!is.null(y)){
     y_name <- deparse(substitute(y))
