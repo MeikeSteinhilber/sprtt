4503f35bfaa5390b7590a9412d0a70d66c3cc9cc
Meike Steinhilber <Meike.Steinhilber@aol.com>
2021-02-17 19:55:20 +0100

    comment out: get_one_sided

---
+++ R/get_one_sample.R
@@ -0,0 +1,19 @@
+get_one_sample <- function(y) {
+  if (is.null(y) || (length(y) == 1)) {
+    one_sample = TRUE
+  } else{
+    one_sample = FALSE
+  }
+  one_sample
+}
+
+# get_one_sided <- function(alternative) {
+#   if (alternative == "two.sided") {
+#     one_sided = FALSE
+#   } else if (alternative == "less" || alternative == "greater"){
+#     one_sided = TRUE
+#   } else {
+#     stop("Argument alternative must be 'two.sided', 'less', or 'greater'.")
+#   }
+#   one_sided
+# }
