75c1617e340efc7381ef466cbe14f2578688c310
Meike Steinhilber <Meike.Steinhilber@aol.com>
2021-03-02 10:50:01 +0100

    add note: @

--- R/show.R
+++ R/show.R
@@ -8,14 +8,15 @@ setMethod("show",
                 ", decision = ", object@decision, sep = "")
 
             cat("\nSPRT thresholds:\n")
-            cat(paste(" lower (B) =", round(object@B_boundary, digits = 5),
-                      "upper (A) =", round(object@A_boundary, digits = 5)))
+            cat(paste(" lower (B) = ", round(object@B_boundary, digits = 5), ", ",
+                      "upper (A) = ", round(object@A_boundary, digits = 5),
+                      sep = ""))
 
             cat("\nLikelihood of the:")
             cat("\n alternative hypothesis =", round(object@likelihood_1, 5))
             cat("\n null hypothesis =", round(object@likelihood_2, 5))
 
-            cat("\n alternative hypothesis: true",
+            cat("\nalternative hypothesis: true",
                 if(object@one_sample == TRUE){
                   if(object@alternative == "two.sided"){
                     paste("mean is not equal to", object@mu, ".")
@@ -36,6 +37,7 @@ setMethod("show",
             cat("\n mean of group one =", round(object@mean_x, 5))
             if(!is.null(object@mean_y)) {
               cat("\n mean of group two =", round(object@mean_y, 5))
-              }
+            }
+            cat("\nNote: to get access to the S4 object (e.g, 'results') use the @ instead of the $ operator, e.g results@mu.")
           })
 
