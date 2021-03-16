b1e9d5e3e10588ab824fe0bcf05fbf7a2cef74a8
MeikeSteinhilber <Meike.Steinhilber@aol.com>
2021-03-16 20:03:22 +0100

    update help: new sprtt man page

---
+++ R/sprtt.R
@@ -0,0 +1,20 @@
+#' sprtt: help page.
+#'
+#' This package provides the implementation of sequential probability
+#' ratio tests using t-statistic.
+#'
+#' @section detailed help pages:
+#' For detailed instructions on the `sprtt` package, see:
+#' [https://meikesteinhilber.github.io/sprtt/index.html]
+#'
+#' @section sprtt functions:
+#' * [`seq_ttest`] Performs one and two sample sequential t-tests on vectors of
+#' data.
+#'
+#' @docType package
+#' @name sprtt
+NULL
+#> NULL
+
+
+
