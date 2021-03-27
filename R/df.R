3c717dfc6e2037304edca6691e2da946b9f4cbe0
Meike <Meike.Steinhilber@aol.com>
2021-03-27 19:51:55 +0100

    update doc

--- R/df.R
+++ R/df.R
@@ -5,10 +5,22 @@
 #'
 #' @format  A data frame with 5 variables and 30 rows
 #' \describe{
-#'   \item{age}{ }
 #'   \item{monthly_income}{ }
-#'   \item{sex}{ male or female}
-#'   \item{whith_children}{ 0 = no and 1 = yes}
 #'   \item{in_debt}{0 = no and 1 = yes}
+#'   \item{sex}{ male or female}
+#'   \item{age}{ }
 #' }
 "df_income"
+
+#' Test data to run the examples
+#'
+#'A dataset that includes 30 individuals.
+#'
+#' @format  A data frame with 4 variables and 30 rows
+#' \describe{
+#'   \item{treatment_group}{ }
+#'   \item{control_group}{ }
+#'   \item{baseline_stress}{ }
+#'   \item{one_year_stress}{ }
+#' }
+"df_stress"
