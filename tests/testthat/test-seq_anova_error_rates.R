b4a7d83aa320b904e71c498e732b59cac130ddc8
MeikeSteinhilber <Meike.Steinhilber@aol.com>
2021-10-25 17:36:49 +0200

    simulation & chatbot

--- tests/testthat/test-seq_anova_error_rates.R
+++ tests/testthat/test-seq_anova_error_rates.R
@@ -1,6 +1,6 @@
 #* @testing seq_anova
 
-test_that("Ceck error rates", {
+test_that("Check error rates", {
   # load packages
   library(testthis)
   library(dplyr)
@@ -11,70 +11,35 @@ test_that("Ceck error rates", {
   # skip("seq_anova simulation. (Takes about 10 minutes)") # skip test
 
   # test setup: simulate the data
-  testthat::source_dir(
-    "testdata-raw/",
-    pattern = "error_rates_anova\\.[rR]$",
-    env = test_env(),
-    chdir = TRUE,
-    wrap = TRUE
-  )
+  # testthat::source_dir(
+  #   "testdata-raw/",
+  #   pattern = "error_rates_anova\\.[rR]$",
+  #   env = test_env(),
+  #   chdir = TRUE,
+  #   wrap = TRUE
+  # )
 
   all_data <- testthis::read_testdata("error_rates_anova_simulation")
 
   effect_data <- all_data %>%
     dplyr::filter(f_simulated != 0)
+  null_effect_data <- all_data %>%
+    dplyr::filter(f_simulated == 0)
 
   # test data reduction: seq_anova must be more efficient than fixed anova
   # expect less than
-  expect_lte(
-    mean(effect_data$sample_size_seq, na.rm = TRUE) /
-    mean(effect_data$sample_size_fixed, na.rm = TRUE),
-    1
-  )
-
   expect_true(
     sum(effect_data$sample_size_seq, na.rm = TRUE) /
-    sum(effect_data$sample_size_fixed, na.rm = TRUE),
-    1
+    sum(effect_data$sample_size_fixed, na.rm = TRUE) < 1
   )
   # at least in 80% of the cases the seq_anova is more efficient
   percent_smaller_samples <- mean(effect_data$sample_smaller)
   expect_true(percent_smaller_samples > 0.80)
 
-  alpha_error_table <- all_data %>%
-    dplyr::filter(f_simulated == 0) %>%
-    dplyr::count(decision)
-  alpha_error_rate <- alpha_error_table$n[2] / sum(alpha_error_table$n)
-
-  # all_data <- all_data %>%
-  #   dplyr::filter(f_simulated == 0) %>%
-  #   dplyr::group_by(f_expected) %>%
-  #   dplyr::mutate(alpha_error_f = mean(decision))
-
-  beta_error_table <- all_data %>%
-    dplyr::filter(f_simulated != 0) %>%
-    dplyr::count(decision)
-  beta_error_rate <- beta_error_table$n[1] / sum(beta_error_table$n)
-
-  error_f_alpha <- all_data %>%
-    dplyr::filter(f_simulated == 0) %>%
-    dplyr::group_by(f_expected) %>%
-    dplyr::mutate(error_f_ = mean(decision)) %>%
-    dplyr::ungroup() %>%
-    dplyr::pull(error_f_)
-
-  error_f_beta <- all_data %>%
-    dplyr::filter(f_simulated != 0) %>%
-    dplyr::group_by(f_expected) %>%
-    dplyr::mutate(error_f_ = 1 - mean(decision)) %>%
-    dplyr::ungroup() %>%
-    dplyr::pull(error_f_)
-
-  all_data <- all_data %>%
-    dplyr::mutate(error_f = c(error_f_alpha, error_f_beta))
+  alpha_error_rate <- mean(null_effect_data$error)
+  beta_error_rate <- mean(effect_data$error)
 
   expect_true(alpha_error_rate < 0.05)
   expect_true(beta_error_rate < 0.05)
-
 
 })
--- tests/testthat/testdata-raw/simulation_error_rates_anova.R
+++ tests/testthat/testdata-raw/simulation_error_rates_anova.R
@@ -1,6 +1,7 @@
 start <- Sys.time()
 library(dplyr)
-
+path_expected <- tempfile(fileext = ".txt")
+sink(path_expected)
 # set simulation parameter -----------------------------------------------------
 set.seed(333)
 
@@ -8,21 +9,30 @@ f_sim <- c(0, 0, 0, 0.1, .25, 0.4)  # 0.1, 0.25, 0.4
 f_exp <- rep(c(0.1, .25, 0.4), 2)
 k_groups <- 4
 max_n <- 20000
-n_rep <- 100
+n_rep <- 10
 alpha <- beta <- .05
 A <- (1 - beta) / alpha
 B <- beta / (1 - alpha)
 seq_steps <- seq((2 * k_groups), (max_n * k_groups), k_groups)
 
+simulated_cases <- n_rep * length(f_sim)
 
-sample_size <- numeric(n_rep * length(f_sim))
-likelihood_ratio <- numeric(n_rep * length(f_sim))
-decision <- numeric(n_rep * length(f_sim))
-f_simulated <- numeric(n_rep * length(f_sim))
-f_expected <- numeric(n_rep * length(f_sim))
-sample_size_fixed <- numeric(n_rep * length(f_sim))
+sample_size <- numeric(simulated_cases)
+likelihood_ratio <- numeric(simulated_cases)
+decision <- numeric(simulated_cases)
+f_simulated <- numeric(simulated_cases)
+f_expected <- numeric(simulated_cases)
 counter <- 1
 j <- 1
+
+# initialize matrix with parameters --------------------------------------------
+
+# matrix <- matrix(nrow = n_rep * length(f_sim), ncol = 5)
+# colnames(matrix) <- c("f_simulated", "f_expected", "LR", "decision", "Sample_size_seq")
+#
+# matrix[, 1] <- sort(rep(f_sim, n_rep))
+# matrix[, 2] <- rep(sort(rep(f_exp, n_rep/2)), 2)
+
 
 # simulation -------------------------------------------------------------------
 for (f in f_sim) {
@@ -44,12 +54,12 @@ for (f in f_sim) {
         decision[counter] <- anova_results@decision
         f_simulated[counter] <- f
         f_expected[counter] <- f_expec
-        if(f != 0) {
-          power_analysis_n <- pwr::pwr.anova.test(k = k_groups, f = f, power = 1 - beta)$n
-          sample_size_fixed[counter] <- round(power_analysis_n * k_groups)
-        } else{
-          sample_size_fixed[counter] <- NA
-        }
+        # if(f != 0) {
+        #   power_analysis_n <- pwr::pwr.anova.test(k = k_groups, f = f, power = 1 - beta)$n
+        #   sample_size_fixed[counter] <- round(power_analysis_n * k_groups)
+        # } else{
+        #   sample_size_fixed[counter] <- NA
+        # }
 
         counter <<- counter + 1
         break
@@ -70,8 +80,7 @@ error_rates_anova_simulation <- data.frame(
   f_expected,
   likelihood_ratio,
   decision,
-  sample_size_seq = sample_size,
-  sample_size_fixed
+  sample_size_seq = sample_size
 )
 
 ## recode decision
@@ -83,15 +92,70 @@ error_rates_anova_simulation$decision <- as.numeric(
 )
 
 # analyse power ----------------------------------------------------------------
+sample_size_fixed <- double(length(unique(f_exp)))
+counter <- 1
+for(f in unique(f_exp)) {
+  power_analysis_n <- pwr::pwr.anova.test(k = k_groups, f = f, power = 1 - beta)$n
+  sample_size_fixed[counter] <- round(power_analysis_n * k_groups)
+  counter <- counter + 1
+}
+sample_size_fixed <- c(rep(NA, simulated_cases/2), sort(rep(sample_size_fixed, n_rep), TRUE))
+
 error_rates_anova_simulation <-
   error_rates_anova_simulation %>%
     dplyr::mutate(
+      sample_size_fixed = sample_size_fixed,
       sample_smaller = .data$sample_size_seq < .data$sample_size_fixed
     )
 
+# detect errors ----------------------------------------------------------------
+alpha_error <-
+  error_rates_anova_simulation %>%
+  dplyr::filter(f_simulated == 0) %>%
+  dplyr::mutate(error = decision == 1) %>%
+  dplyr::pull(.data$error)
+beta_error <-
+  error_rates_anova_simulation %>%
+  dplyr::filter(f_simulated != 0) %>%
+  dplyr::mutate(error = decision == 0) %>%
+  dplyr::pull(.data$error)
+
+error_rates_anova_simulation <-
+  error_rates_anova_simulation %>%
+  dplyr::mutate(error = c(alpha_error, beta_error))
+
+# calculate XXXX ---------------------------------------------------------------
+error_f_alpha <- error_rates_anova_simulation %>%
+  dplyr::filter(f_simulated == 0) %>%
+  dplyr::group_by(f_expected) %>%
+  dplyr::mutate(error_f_ = mean(error)) %>%
+  dplyr::ungroup() %>%
+  dplyr::pull(error_f_)
+
+error_f_beta <- error_rates_anova_simulation %>%
+  dplyr::filter(f_simulated != 0) %>%
+dplyr::group_by(f_expected) %>%
+  dplyr::mutate(error_f_ = mean(error)) %>%
+  dplyr::ungroup() %>%
+  dplyr::pull(error_f_)
+
+error_rates_anova_simulation <- error_rates_anova_simulation %>%
+  dplyr::mutate(error_f = c(error_f_alpha, error_f_beta))
+
+error_rates_anova_simulation <- error_rates_anova_simulation %>%
+  dplyr::group_by(f_expected, f_simulated) %>%
+  dplyr::mutate(mean_sample_seq_f = round(mean(.data$sample_size_seq), 0)) %>%
+  dplyr::ungroup()
+
+
+anova_performance <- error_rates_anova_simulation %>%
+  select(f_simulated, f_expected, mean_sample_seq_f, sample_size_fixed, error_f) %>%
+  group_by(f_simulated, f_expected, mean_sample_seq_f, sample_size_fixed, error_f) %>%
+  summarise()
 
 # save simulation results ------------------------------------------------------
 testthis::use_testdata(error_rates_anova_simulation, overwrite = TRUE)
+testthis::use_testdata(anova_performance, overwrite = TRUE)
 
 
 ##---- UNDO SET.SEED -----------------------------------------------------------
@@ -100,4 +164,34 @@ set.seed(NULL)
 
 
 end <- Sys.time()
-print(end - start)
+duration <- end - start
+duration
+
+# analyse performance ----------------------------------------------------------
+# profvis::profvis(source("tests/testthat/testdata-raw/simulation_error_rates_anova.R"))
+
+
+# sent results to Telegram Bot -------------------------------------------------
+sink(file = NULL) # STOP sink
+
+# install.packages("telegram.bot")
+library(telegram.bot)
+
+#
+user_name = "mst_r_bot"
+token = bot_token("RTelegramBot")
+
+# Initialize bot
+bot <- Bot(token = token)
+chat_id = 1084175541
+
+console <- toString(read.csv(path_expected, sep = ";", header = FALSE))
+# write.table(console, path_expected,sep="\t",row.names=TRUE)
+# console <- read.delim(path_expected)
+
+
+bot$sendMessage(chat_id = chat_id, text = "finished: simulation_error_rates_anova")
+bot$sendMessage(chat_id = chat_id, text = paste("Consol Output>>>>>>>>>>", console))
+# bot$sendMessage(chat_id = chat_id, text = duration)
+
+
