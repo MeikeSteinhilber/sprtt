a8425b88554ed4c83ba7b083507506083093fa63
Meike <Meike.Steinhilber@aol.com>
2022-04-22 10:59:08 +0200

    add show() anova and tests

---
+++ R/draw_sample_anova.R
@@ -0,0 +1,9 @@
+draw_sample <- function(k_groups = 3, f, sd = c(1, 1, 1), max_n = 50) {
+  if(k_groups != length(sd)) {stop(" k_groups needs to be equal to length of sd.")}
+  raw_means <- rnorm(k_groups)
+  f_new <- f*sqrt(sum(sd^2)/k_groups)
+  means = (raw_means - mean(raw_means)) / sd(raw_means) * sqrt(k_groups / (k_groups - 1)) * f_new
+  y <- rnorm(max_n * k_groups, means, sd = sd)
+  x <- factor(rep(1:k_groups, max_n))
+  data.frame(y, x)
+}
